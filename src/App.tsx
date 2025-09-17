9e9d3487aeac093f523f5900000462f4688e996e
axiarena <axiframework@gmail.com>
2025-09-17 14:01:58 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -586,7 +586,7 @@ function App() {
                 }}
               >
                 <div className="text-sm font-bold">
-                   6ix Fold AGI Matrix Portal
+                   6ixFold AGI Matrix Portal
                 </div>
                 <div className="text-xs mt-2 opacity-80">
                   System Pressure: 100% | Modules: 6 | Interface: ONLINE
