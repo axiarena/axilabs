a71ccad96d8741bc20e5336e65b0dec4c1498c8b
axiarena <axiframework@gmail.com>
2025-09-17 15:06:45 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -515,7 +515,7 @@ function App() {
                         boxShadow: '0 0 15px rgba(138, 43, 226, 0.5)'
                       }}
                     >
-                     Reasoning Arc 
+                     Singularity Arc 
                     </div>
                   </div>
                 </div>
