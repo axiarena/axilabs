1cdd091522904ee7631b665ee97a9da1f7153a0a
axiarena <axiframework@gmail.com>
2025-09-17 14:35:05 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -513,7 +513,7 @@ function App() {
                         boxShadow: '0 0 15px rgba(0, 255, 136, 0.5)'
                       }}
                     >
-                     Neural Clone Mapping
+                    
                     </div>
                     
                     {/* Brain visualization with clone nodes */}
