2ef5b0b52ce08efee2df22624bddf8c2681dc27b
axiarena <axiframework@gmail.com>
2025-12-02 20:37:23 -0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -515,7 +515,7 @@ function App() {
                         boxShadow: '0 0 15px rgba(138, 43, 226, 0.5)'
                       }}
                     >
-                      Trading Card Game (TCH)
+                      Trading Card Game (TCG)
                     </div>
                   </div>
                 </div>
